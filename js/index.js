1d5c49bfe245af6e5a6ce70c0552e27c56458050
ahasan2912 <habib15-4284@diu.edu.bd>
2024-09-19 01:04:03 +0600

    added home page

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//search: form submit reloading the page
+//step-1
+document.getElementById('button-login').addEventListener('click', function(event){
+    //step-2
+    event.preventDefault(); //browser ke laod hote dei na;
+
+    //step-3 get the phone number;
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('Pin number wrong');
+    }
+});
