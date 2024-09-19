c76fd792fafbbbe1ef19a2a090e5b5af676b87a9
ahasan2912 <habib15-4284@diu.edu.bd>
2024-09-19 07:40:19 +0600

    update

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ document.getElementById('button-login').addEventListener('click', function(event
     const pinNumber = document.getElementById('pin-number').value;
 
     if(phoneNumber === '5' && pinNumber === '1234'){
-        window.location.href = '/home.html';
+        window.location.href = './home.js';
     }
     else{
         alert("Worng Pin Number");
