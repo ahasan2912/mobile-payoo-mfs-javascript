b1e849ba2bbe51932ab2a70dee57cab40acb0298
ahasan2912 <habib15-4284@diu.edu.bd>
2024-09-19 08:38:13 +0600

    add some some feature

---
+++ js/features.js
@@ -0,0 +1,12 @@
+// show the cash out form 
+document.getElementById('show-cash-out').addEventListener('click', function(){
+    document.getElementById('cash-out-form').classList.remove('hidden');
+    document.getElementById('add-money-form').classList.add('hidden');
+});
+
+//show the cash out form
+document.getElementById('show-add-money').addEventListener('click', function(){
+    document.getElementById('add-money-form').classList.remove('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+});
+
